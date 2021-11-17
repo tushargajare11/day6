9bc564a7a335111970261b5949621e08fa455561
tushargajare11 <tushargajare119@gamil.com>
2021-11-17 15:46:17 +0530

    Prime number

---
+++ PrimeNum.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+read -p "Enter the number" n
+
+for ((i=2;i<=$n/2;i++))
+do
+	if [ $((n%i)) -eq 0 ]
+	then
+	echo "$n is not a prime number"
+
+exit
+fi
+
+done
+	echo "$n is a prime number"
+
